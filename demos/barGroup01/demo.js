340ecadcda55b4035a5f4940137b321105075c0e
wooritech <jun@wrw.kr>
2014-01-16 17:52:09 +0900

    original

--- demos/barGroup01/demo.js
+++ demos/barGroup01/demo.js
@@ -1,69 +1,115 @@
 $(function () {
-        $('#container').highcharts({
-            chart: {
-                type: 'area'
-            },
-            title: {
-                text: 'US and USSR nuclear stockpiles'
-            },
-            subtitle: {
-                text: 'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">'+
-                    'thebulletin.metapress.com</a>'
-            },
-            xAxis: {
-                labels: {
-                    formatter: function() {
-                        return this.value; // clean, unformatted number for year
-                    }
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Nuclear weapon states'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value / 1000 +'k';
-                    }
-                }
-            },
-            tooltip: {
-                pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
-            },
-            plotOptions: {
-                area: {
-                    pointStart: 1940,
-                    marker: {
-                        enabled: false,
-                        symbol: 'circle',
-                        radius: 2,
-                        states: {
-                            hover: {
-                                enabled: true
-                            }
-                        }
-                    }
-                }
-            },
-            series: [{
-                name: 'USA',
-                data: [null, null, null, null, null, 6 , 11, 32, 110, 235, 369, 640,
-                    1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
-                    27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
-                    26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-                    24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
-                    22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
-                    10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104 ]
-            }, {
-                name: 'USSR/Russia',
-                data: [null, null, null, null, null, null, null , null , null ,null,
-                5, 25, 50, 120, 150, 200, 426, 660, 869, 1060, 1605, 2471, 3322,
-                4238, 5221, 6129, 7089, 8339, 9399, 10538, 11643, 13092, 14478,
-                15915, 17385, 19055, 21205, 23044, 25393, 27935, 30062, 32049,
-                33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000, 37000,
-                35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-                21000, 20000, 19000, 18000, 18000, 17000, 16000]
-            }]
-        });
-    });
-    
+	new ChartWeb.ChartView('chartviewhtml',
+		 {
+			"type": "bar",
+			"palettes":[{
+				"name": "blue",
+				"colors":"#0080e0"
+				}, {
+				"name": "black",
+				"colors":"#000000"
+				}, {
+				"name": "green",
+				"colors":"#aadf10"
+				}
+			],				
+			"title": {
+				"caption": "하반기 사원별 판매 실적 현황",
+				"styles": {
+					"textColor": "#f00000"
+				},
+				"subTitle":{
+					"position": "bottom",
+					"text": "목표: 전월대비 10% 이상",
+					"styles": {
+						"textAlign": "center",
+						"textColor": "#2ab6da"				
+					}
+				}
+			},
+			"yAxes": {
+				"title": {
+					"styles":{
+						"visible": false
+					}
+				},
+				"categories": ["컴퓨터", "모니터", "키보드", "마우스", "모니터받침대"]
+			},
+			"xAxes": {
+				"title": {
+					"caption": "실제 판매실적",
+					"styles":{
+						"textColor": "#389f2c"
+					}
+				},
+				"baseValue": 0,
+				"styles": {
+					"grid": {
+						"stroke": "#18000000"
+					}
+				}
+			},
+			"seriesGroups": {
+				"name": "group1",
+				"stackMode": "normal",
+				"label": {
+					"styles": {
+					},
+					"items": [
+					]
+				}
+			},
+			"series": [{
+				"name": "영업사원1",
+				"group": "group1",
+				"items": [17, 13, 16, 13, 17],
+				"styles": {
+					"fill": "pal(blue)",
+					"fillAlpha": 0.7,
+					"strokeAlpha": 1,
+					"label": {
+						"horizontalAlign": "left",
+						"textColor": "#eeeeee"
+					}
+				},
+				"animations": [{
+					"property": "mask"
+				}]
+				}, {
+				"name": "영업사원2",
+				"group": "group1",
+				"items": [20, 20, 20, 20, 20],
+				"styles": {
+					"fill": "pal(black)",
+					"fillAlpha": 0.7,
+					"strokeAlpha": 1,
+					"label": {
+						"horizontalAlign": "left",
+						"textColor": "#dddddd"
+					}
+				},
+				"animations": [{
+					"property": "mask"
+				}]
+				}, {
+				"name": "영업사원3",
+				"group": "group1",
+				"items": [13, 11, 20, 12, 11],
+				"styles": {
+					"fill": "pal(green)",
+					"fillAlpha": 0.7,
+					"strokeAlpha": 1,
+					"label": {
+						"horizontalAlign": "left",
+						"textColor": "#008800"
+					}
+				},
+				"animations": [{
+					"property": "mask"
+				}]
+			}],
+			"legend": {
+			}
+		}
+	);
+});
